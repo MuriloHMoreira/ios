6fb10a60ee83bcddb81370285512fb9fde56f330
Murilo Henrique Moreira <moreira.murilo@gmail.com>
2024-01-05 10:29:22 -0300

    cleaning dev branch

--- make.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-pelican -lr
-pause
--- push.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-pelican content -s publishconf.py
-set /p message="Commit Message: "
-ghp-import output -b master -m %message%
-git push origin master
